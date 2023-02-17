6dc2ec5429ee18dd7ce6bed89650376ce2ea1ff3
timrusin <timrusin@gmail.com>
2023-02-17 13:45:57 -0600

    added some base classes in index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,28 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    //if you needed to build a custom "design system" you can add new themes
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+
     extend: {
+      //first customization simply creating a new color:
       // colors: {
-      //   spanky: "rgb(240, 114, 10)"
-      // }
+      //   spanky: "rgb(240, 114, 10)" 
+      // } 
       colors: {
         spanky: {
           50: "rgb(240, 114, 10, .25)",
