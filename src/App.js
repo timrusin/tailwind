abb8421559d70dbdc7d78c659ad472d9a3f44a8b
timrusin <timrusin@gmail.com>
2023-02-17 14:56:18 -0600

    playing arond with flex a bit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 function App() {
   return (
-    <h1 className="text-center sm:text-3xl lg:text-5xl xl:text-8xl bg-gray-200 border-2 border-green-400">
-      Hello world!
-    </h1>
+    <div className="flex text-center sm:text-3xl lg:text-5xl xl:text-8xl bg-gray-200 ">
+      <h1 className="basis-1/2 order-2 border-2 border-green-400">
+        Hello world!
+      </h1>
+      <h1 className="basis-1/2 order-1 border-2 border-green-400">
+        test
+      </h1>
+    </div>
   );
 }
 
