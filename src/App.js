3c0db048556fbffc7a57c87a14de211d7c12d1f9
timrusin <timrusin@gmail.com>
2023-02-17 13:14:21 -0600

    created media querys for sm lg xl of centered text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-5xl bg-gray-200 text-spanky-400 border-2 border-green-400">
+    <h1 className="text-center sm:text-3xl lg:text-5xl xl:text-8xl bg-gray-200 text-spanky-400 border-2 border-green-400">
       Hello world!
     </h1>
   );
