9db6eb2d96ecd323f13c04b4e253b13f83742c87
timrusin <timrusin@gmail.com>
2023-02-17 12:55:45 -0600

    text color bg and border

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-5xl font-bold underline text-green-300">
+    <h1 className="text-5xl bg-gray-200 text-blue-400 border-2 border-green-400">
       Hello world!
     </h1>
   );
