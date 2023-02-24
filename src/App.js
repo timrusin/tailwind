68c05184b8c69242772ebc0fe34e0c80fce06acd
timrusin <timrusin@gmail.com>
2023-02-23 21:30:58 -0600

    playing around with and refreshing on callback onclick funciton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 function App() {
+
+  const clg = (value) => {
+    console.log(`${value} clicked`)
+  }
+
   return (
     <div className="flex text-center m-4 xs:text-sm sm:text-base lg:text-3xl xl:text-5xl bg-gray-200">
-        <h1 className="basis-1/2 m-2 order-1 border-2 bg-red-200 hover:bg-yellow-200 ease-in duration-200 border-green-400">
+        <h1 className="basis-1/2 m-2 order-1 border-2 bg-red-200 hover:bg-yellow-200 ease-in duration-200 border-green-400" onClick={() => clg("Hello")}>
           Hello world!
         </h1>
-        <h1 className="basis-1/3 m-2 order-3 border-2 bg-blue-200 hover:bg-yellow-200 ease-in duration-200 border-green-400">
+        <h1 className="basis-1/3 m-2 order-3 border-2 bg-blue-200 hover:bg-yellow-200 ease-in duration-200 border-green-400" onClick={() => clg("Test")}>
           test
         </h1>
-        <h1 className="basis-1/3 m-2 order-2 border-2 bg-purple-200 hover:bg-yellow-200 ease-in duration-200 border-green-400">
+        <h1 className="basis-1/3 m-2 order-2 border-2 bg-purple-200 hover:bg-yellow-200 ease-in duration-200 border-green-400" onClick={() => clg("Birds")}>
           Birds
         </h1>
     </div>
